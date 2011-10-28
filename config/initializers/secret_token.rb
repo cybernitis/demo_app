2de807cd8f5872af8e346e38832c8b36f30172ef
cybernitis <git@cybernitis.net>
2011-10-28 01:51:47 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9bd5cdb7b3a425a53cf15e2f28b164cf3026ddbfb7dfa67781a857861468a8dbd1ca71d83628baff9ccf53f9b1806a68ec0479ef1f2557c796651013e8f26df8'
